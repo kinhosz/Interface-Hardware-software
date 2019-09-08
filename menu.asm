5ed1a647e1dab62b2af64f098dc0d7e96a07f63e
José Carlos <39632709+kinhosz@users.noreply.github.com>
2019-09-07 22:18:25 -0300

    Finalizado

--- menu.asm
+++ menu.asm
@@ -1,379 +1,446 @@
-;inicio zenio
-;Tetris: O Classico
 org 0x7E00
-jmp 0x0000:start
+jmp 0x0000:main
 
-;Menu Principal
-titulo db '  Tetris', 13
-classico db 'O Classico', 13
-jogar db ' Jogar', 13
-instrucoes db ' Instrucoes/Creditos', 13
+data:
 
-;Instruções
-tituloInstrucoes db 'Instrucoes', 13
-tituloObjetivo db 'Objetivo: ', 13
-objetivo db 'Preencher o menor `espaco vertical` possivel.', 13
-com db 'Comandos:', 13
-comando1 db '-> tecla `d` para rotacionar a figura para a direita', 13
-comando2 db '-> tecla `a` para rotacionar a figura para a esquerda', 13
-vai db ' Ir para o Jogo', 13
+	banco times 400 db 0
+	input times 2 db 0
+	;######### menu ############
+	cadastrar db '3 - cadastrar conta',13,10,0
+	buscar db '4 - Buscar conta:',13,10,0
+	editarConta db '5 - Editar conta:',13,10,0
+	deletar db '6 - Deletar conta:',13,10,0
+	exite db 'qualquer outra coisa - sair',13,10,0
+	conta_nao_existe db 'Esta conta nao esta cadastrada',13,10,0
+	;########## fim menu #############
+	;##############warnings#################
+	w_deletar db 'Digite o numero da conta entre 0 e 9',13,10,0 
+	w_conta_deletada db 'Conta deletada com sucesso!',13,10,0
+	fim_banco db 'Banco Fechado, ate mais!',13,10,0
+	endl db ' ',13,10,0
+	bankisfull db 'Nao eh possivel adicionar mais uma conta, banco esta cheio!',13,10,0
+	digiteNomeUser db 'Digite o nome do usuario:',13,10,0
+	string times 30 db 0 ; string para ler entrada de dados
+	addSuccessfull db 'Conta adicionada. Numero da conta:',13,10,0
+	digiteCpf db 'Digite o cpf (sem tracos):',13,10,0	
+	nomeUser db  'Nome: ',0 ; sem o endl intencionalmente
+	cpfUser db 'CPF: ',0 ; sem o endl intencionalmente
+	contaEncontrada db 'Conta Encontrada',13,10,0
+	contaNaoCadastrada db 'Conta Nao Encontrada.',13,10,0
+	contaEditada db 'Alteracoes realizadas com sucesso.',13,10,0
+	space db '                                               ',13,10,0
+	;########################################
+	
 
-;Créditos
-cred db 'Creditos', 13
-membro1 db '-Jose Carlos(jcsc)', 13
-membro2 db '-Alexsandro Jose(ajs6)', 13
-membro3 db '-Zenio Angelo(zaon)', 13
 
-start:
-	; Modo vídeo
-	mov ah, 0
-	mov al, 12h
+
+
+getchar:
+  mov ah, 0x00
+  int 16h
+  ret
+
+
+putchar:
+	mov ah,0x0e
 	int 10h
+	ret
+	
+prints:             ; mov si, string
+  .loop:
+    lodsb           ; bota character em al 
+    cmp al, 0
+    je .endloop
+    call putchar
+    jmp .loop
+  .endloop:
+  ret
 
-	call menuPrincipal
+clear: ; essa funcao limpa a tela e posiciona o cursor na posicao (0,0)
 
-	jmp exit
+	mov cx,30
+	.for:
+		dec cx
+		cmp cx,0
+		je .fimloop
+		mov si,endl
+		call prints
+		jmp .for
+		.fimloop:
+	
+		mov ah,02h
+		mov dh,0
+		mov dl,0
+		int 10h
+	
+	ret
 
-menuPrincipal:
+check: ; retorna flag no ah caso exista
+	  ; bx com o offset
 
-	;Colorindo a tela de verde
-	mov ah, 0xb  
-	mov bh, 0     
-	mov bl, 2
-	int 10h 
-
-	;Setando o cursor
-	mov ah, 02h
-	mov bh, 00h
-	mov dh, 07h
-	mov dl, 20h
-	int 10h
-
-	mov si, titulo
-	printTitulo:
+	.for:
+		mov si,banco ; si = offset
+		add si,bx ; bx = pos no array
 		lodsb
-		mov ah, 0xe
-		mov bh, 0
-		mov bl, 4
-		int 10h
-		call delay
-		cmp al, 13
-		jne printTitulo
-
-	call print_subTitulo
-
-	call menu
-
-	cmp cx, 2
-	je telaInstCred
-
-	call jogo
-
-	telaInstCred:
-
-		;Modo vídeo novamente para limpar a tela
-		mov ah, 0
-		mov al, 12h
-		int 10h
-
-		;Colorindo novamente a tela de verde
-		mov ah, 0xb  
-		mov bh, 0     
-		mov bl, 2   
-		int 10h
-
-		;Instruções
-		mov ah, 02h
-		mov bh, 00h
-		mov dh, 02h
-		mov dl, 22h
-		int 10h
-
-		mov bl, 0xe
-		mov si, tituloInstrucoes
-		call printString
-
-		;Objetivo
-		mov ah, 02h
-		mov bh, 00h
-		mov dh, 05h
-		mov dl, 01h
-		int 10h
-		mov si, tituloObjetivo
-		mov bl, 0xe
-		call printString
-
-		mov ah, 02h
-		mov bh, 00h
-		mov dh, 05h
-		mov dl, 11
-		int 10h
-		mov si, objetivo
-		mov bl, 0xf
-		call printString
+		dec si
+		cmp al,0
+		je .achei ; achei uma posicao disponivel
+		add bx,35
+		cmp bx,350
+		je .fim
+		jmp .for
+	.achei:	
+		mov ah,1 ; seta register de validade
+		mov di,banco ; di = offset
+		add di,bx ; bx = pos
+		mov al,1 ; ocupa aquela conta
+		stosb
 		
-		;Comandos
-		mov ah, 02h
-		mov bh, 00h
-		mov dh, 08h
-		mov dl, 01h
-		int 10h
-		mov si, com
-		mov bl, 0xe
-		call printString
-
-		mov ah, 02h
-		mov bh, 00h
-		mov dh, 08h
-		mov dl, 11
-		int 10h
-		mov si, comando1
-		mov bl, 0xf
-		call printString
-
-		mov ah, 02h
-		mov bh, 00h
-		mov dh, 09h
-		mov dl, 11
-		int 10h
-		mov si, comando2
-		mov bl, 0xf
-		call printString
-
-		;Creditos
-		mov ah, 02h
-		mov bh, 00h
-		mov dh, 11
-		mov dl, 22h
-		int 10h
-		mov si, cred
-		mov bl, 0xe
-		call printString
-		
-		mov ah, 02h
-		mov bh, 00h
-		mov dh, 12
-		mov dl, 01h
-		int 10h
-		mov si, membro1
-		mov bl, 0xf
-		call printString
-
-		mov ah, 02h
-		mov bh, 00h
-		mov dh, 13
-		mov dl, 01h
-		int 10h
-		mov si, membro2
-		mov bl, 0xf
-		call printString
-
-		mov ah, 02h
-		mov bh, 00h
-		mov dh, 14
-		mov dl, 01h
-		int 10h
-		mov si, membro3
-		mov bl, 0xf
-		call printString
-
-		;Ir para o jogo
-		mov ah, 02h
-		mov bh, 00h
-		mov dh, 26
-		mov dl, 01h
-		int 10h
-
-		mov ah, 0xe
-		mov al, 175
-		mov bh, 0
-		mov bl, 0xe
-		int 10h
-
-		mov si, vai
-		call printString
-
-		esperaEnter:
-			mov ah, 0
-			int 16h
-			cmp al, 13
-			jne esperaEnter	
-
-	call jogo	
-			
-ret
-menu:
-	mov ah, 02h
-	mov bh, 00h
-	mov dh, 10h
-	mov dl, 10h
-	int 10h
-
-	mov ah, 0xe
-	mov al, 175
-	mov bh, 0
-	mov bl, 4
-	int 10h
-
-	mov si, jogar
-	call printString
-
-	mov ah, 02h
-	mov bh, 00h
-	mov dh, 12h
-	mov dl, 11h
-	int 10h
-
-	mov si, instrucoes
-	call printString
-
-	mov cx, 1 ;CX contém a posição da seta
-	call mudancaSeta
-
-	
-ret
-
-mudancaSeta:
-	
-	mov ah, 0
-	int 16h
-
-	cmp al, 's'
-	je Baixo
-
-	cmp al, 'w'
-	je Cima
-
-	cmp al, 13
-	jne mudancaSeta
+	.fim:
 
 	ret
 
-	Baixo: ;Faz o apontador se deslocar para baixo
-		cmp cx, 2
-		je Cima
+gets: ; leitor de string
+	 ; obs.: se voce errar a string, o problema eh seu
+	mov al,0
+	.for:
+		call getchar ; le caractere
+		stosb
+		cmp al,13
+		je .fim
+		call putchar ; printa a letra
+		jmp .for
+	.fim:
+	dec di
+	mov al,0
+	stosb
+	mov si,endl
+	call prints		
+	ret
 
-		mov ah, 02h
-		mov bh, 00h
-		mov dh, 10h
-		mov dl, 10h
-		int 10h
+armz: ; armazenando na memoria a string recebida na posicao bx
 
-		mov ah, 0xe
-		mov al, 0
-		mov bh, 0
-		mov bl, 4
-		int 10h
+	mov di,banco
+	mov si,string
+	add di,bx ; di -> posicao que irei escrever, si -> posicao que irei ler
 
-		mov ah, 02h
-		mov bh, 00h
-		mov dh, 12h
-		mov dl, 10h
-		int 10h
+	.for:
+		lodsb
+		stosb
+		cmp al,0
+		je .fim
+		jmp .for
+	.fim:
 
-		mov ah, 0xe
-		mov al, 175
-		mov bh, 0
-		mov bl, 4
-		int 10h
+	ret
+		
 
-		mov cx, 2
-		jmp mudancaSeta
+cadastrar_conta: 
+	call clear
 
-	Cima: ;Faz o apontador se deslocar para cima
-		cmp cx, 1
-		je Baixo
+	; existe espaco para mais uma conta ?
+	mov ah,0
+	mov bx,0
+	call check
+	cmp ah,0
+	je .naopossuiespaco ; nao ha espaco, entao nao sera possivel adicionar
 
-		mov ah, 02h
-		mov bh, 00h
-		mov dh, 10h
-		mov dl, 10h
-		int 10h
+	 ; possui espaco
+		mov si,digiteNomeUser ;
+		call prints
+		mov di,string
+		call gets ; ler nome do usuario
+		inc bx  ; bx indica em qual posicao no vetorzao colocar a string
+		call armz ; armazenando na memoria
+		mov si,digiteCpf ; mensagem para printar cpf
+		call prints
+		mov di,string
+		call gets ; lendo cpf (sem tracos)
+		mov cx,20
+		add bx,cx 
+		call armz ; armazenando cpf na memoria
+		call clear
+		mov si,addSuccessfull ; printa mensagem de sucesso
+		call prints
+		; descobrindo o numero da conta
+		mov dx,0
+		mov ax,bx
+		mov bx,35
+		div bx
+		mov bl,48
+		add al,bl
+		call putchar ; printa numero da conta
+		mov si,endl
+		call prints
+		mov si,endl
+		call prints		
 
-		mov ah, 0xe
-		mov al, 175
-		mov bh, 0
-		mov bl, 4
-		int 10h
+	jmp .fim
+	
+	.naopossuiespaco: ; informando ao usuario que nao ha espaco
+		call clear
+		mov si,bankisfull 
+	 	call prints	
+		mov si,endl
+		call prints
+	
+	.fim:
 
-		mov ah, 02h
-		mov bh, 00h
-		mov dh, 12h
-		mov dl, 10h
-		int 10h
+	jmp voltei
 
-		mov ah, 0xe
-		mov al, 0
-		mov bh, 0
-		mov bl, 4
-		int 10h
+buscar_conta:
+	call clear
 
-		mov cx, 1
-		jmp mudancaSeta
+	mov si,w_deletar
+	call prints
+	mov si,endl
+	call prints
 
-print_subTitulo: ;Seta a posição onde "O Clássico" será printado e deixa a palavra vermelha
-	mov ah, 02h
-	mov bh, 00h
-	mov dh, 08h
-	mov dl, 20h
-	int 10h
-	mov bl, 4
-	mov si, classico
-	call printClassico
-	call delay
+	call getchar
+	call putchar
+	sub al,48
+	mov cl,al
+	mov si,endl
+	call prints
+	mov si,endl
+	call prints
+	mov bx,0 ; bx = pos
 
-ret
+	; find conta
+	mov si,banco
+	inc cl
+	.for: ; move si para o num da conta
+		dec cl
+		cmp cl,0
+		je .edit
+		add si,35
+		add bx,35
+		jmp .for
+	.edit:
+		lodsb
+		cmp al,0
+		je .if
 
-printClassico: ;Printa a palavra "O Clássico"
-	lodsb
-	mov ah, 0xe
-	mov bh, 0
-	int 10h
-	cmp al, 13
-	jne printClassico
-ret
+	; se chegou aqui, entao a conta existe
+	mov si,contaEncontrada ; 
+	call prints
+	mov si,endl
+	call prints
+	mov si,endl
+	call prints
+	mov si,nomeUser ; nome do dono da conta
+	call prints
+	mov si,banco
+	add si,bx
+	inc si
+	call prints
+	mov si,endl
+	call prints
+	mov si,cpfUser ; cpf do usuario
+	call prints
+	mov si,banco
+	add si,21
+	call prints
+	mov si,endl
+	call prints
+	mov si,endl
+	call prints
+	jmp .else
 
-printString: ;Printa uma palavra ou frase na tela
-	lodsb
-	mov ah, 0xe
-	mov bh, 0
-	int 10h
-	cmp al, 13
-	jne printString
-ret
+	.if:
+	mov si,contaNaoCadastrada ; informa que a conta nao foi cadastrada
+	call prints
+	mov si,endl
+	call prints
+	mov si,endl
+	call prints
+	.else:
+	
+	jmp voltei
 
-delay: ;Simula uma pausa na execução
-	mov bp, 350
-	mov dx, 350
-	delay2:
-		dec bp
-		nop
-		jnz delay2
-	dec dx
-	jnz delay2
+editar_conta:
+	call clear
+	
+	mov si,w_deletar ; printf(Digite numero da conta)
+	call prints
+	mov si,endl
+	call prints
 
-ret
+	call getchar
+	call putchar
+	sub al,48
+	mov cl,al
+	mov si,endl
+	call prints
+	mov si,endl
+	call prints
+	mov bx,0 ; bx = pos
 
-jogo:
-;Setando a posição do disco onde kernel.asm foi armazenado(ES:BX = [0x500:0x0])
-	mov ax,0x860		;0x50<<1 + 0 = 0x500
+	; find conta
+	mov si,banco
+	inc cl
+	.for: ; move si para o num da conta
+		dec cl
+		cmp cl,0
+		je .edit
+		add si,35
+		add bx,35
+		jmp .for
+	.edit:
+		lodsb
+		cmp al,0
+		je .if
+
+	; se chegou aqui, entao a conta existe
+	mov si,contaEncontrada ; 
+	call prints
+	mov si,endl
+	call prints
+
+	mov si,digiteNomeUser ;
+	call prints
+	mov di,string
+	call gets ; ler nome do usuario
+	inc bx  ; bx indica em qual posicao no vetorzao colocar a string
+	call armz ; armazenando na memoria
+	mov si,digiteCpf ; mensagem para printar cpf
+	call prints
+	mov di,string
+	call gets ; lendo cpf (sem tracos)
+	mov cx,20
+	add bx,cx 
+	call armz ; armazenando cpf na memoria
+	call clear
+	mov si,contaEditada ; printa mensagem de sucesso
+	call prints
+	mov si,endl
+	call prints
+	jmp .else
+
+	.if:
+	mov si,contaNaoCadastrada ; informa que a conta nao foi cadastrada
+	call prints
+	mov si,endl
+	call prints
+	mov si,endl
+	call prints
+	.else:
+	
+	jmp voltei
+
+deletar_conta:
+	mov si,endl
+	call prints
+	
+	mov si,w_deletar ; informe o numero da conta
+	call prints
+	mov si,endl
+	call prints
+
+	call getchar
+	mov ch,al
+	mov cl,0	
+	sub ch,48
+	mov si,banco
+	mov di,banco
+	inc ch
+	; ch -> conta a ser deletada
+
+	.loop1:
+		dec ch
+		cmp ch,0
+		je .edit
+		add si,35
+		add si,banco
+		jmp .loop1
+			
+	.edit:
+		lodsb
+		mov ah,al
+		push ax
+		mov al,0
+		
+		stosb
+
+	call clear
+	pop ax
+	cmp ah,0
+	je .if
+	mov si,w_conta_deletada ; se a contra existir, deleta a conta
+	call prints
+	jmp .else
+	.if:
+		mov si, conta_nao_existe ; se a conta nao existir, nao deleta a conta
+		call prints
+	.else:
+		mov si,endl
+		call prints
+		mov si,endl
+		call prints
+
+	jmp voltei
+
+loop_main:
+
+	
+	loop:
+	;######### MENU ###################
+	mov si,cadastrar
+	call prints
+	mov si, buscar
+	call prints
+	mov si,editarConta
+	call prints
+	mov si,deletar
+	call prints	
+	mov si,exite
+	call prints
+		
+	;###### processa##################################
+	call getchar
+	push ax
+	call putchar
+	mov si,endl
+	call prints
+	pop ax
+
+	cmp al,'3'
+	je cadastrar_conta
+
+	cmp al,'4'
+	je buscar_conta
+
+	cmp al,'5'
+	je editar_conta
+
+	cmp al,'6'
+	je deletar_conta
+	
+	jmp exit
+
+	mov si,endl
+	call prints
+	voltei:
+	jmp loop
+	exit:
+	ret
+
+main:
+
+	xor ax,ax
+	mov ds,ax
 	mov es,ax
-	xor bx,bx		;Zerando o offset
-
-;Setando a posição da Ram onde o jogo será lido
-	mov ah, 0x02	;comando de ler setor do disco
-	mov al,8		;quantidade de blocos ocupados por jogo
-	mov dl,0		;drive floppy
-
-;Usaremos as seguintes posições na memoria:
-	mov ch,0		;trilha 0
-	mov cl,7		;setor 2
-	mov dh,0		;cabeca 0
-	int 13h
-	jc jogo	;em caso de erro, tenta de novo
-
-break:	
-	jmp 0x8600 		;Pula para a posição carregada
 
 
-exit:
+	call loop_main  ; loop infinito que roda o banco
+	mov si,fim_banco ; mensagem de despedida
+	call prints
+	;jmp finish
+
+finish:
+
+	mov ah,4ch
+	int 21h ; encerrando o codigo
+; one 1
+; two 2
+  
